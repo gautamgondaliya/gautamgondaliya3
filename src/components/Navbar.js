e7ff940778a8df9e370b6c7d8878d6371eb0c300
gautamgondaliya <97119817+gautamgondaliya@users.noreply.github.com>
2024-11-06 19:50:10 +0530

    Updated Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/gautamgondaliya/gautamgondaliya311"
+                href="https://github.com/gautamgondaliya/"
                 target="_blank"
                 className="fork-btn-inner"
               >
